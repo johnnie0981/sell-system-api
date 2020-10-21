69d62687f8eb1327df8e5c7cdc9178899a70e225
johnnie0981_dev <monzar0981@gmail.com>
2020-10-21 19:48:21 +0700

    add update status

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var { get_invoice } = require("./invoice");
 var { get_invioce_detail } = require("./invoice");
+var { update_status } = require("./invoice");
 var guard = require("../controllers/check_token");
 
 /* GET home page. */
@@ -12,5 +13,7 @@ router.get("/", function (req, res, next) {
 router.get("/get/invoice", guard, get_invoice);
 
 router.get("/get/invoice_detail/:invid", guard, get_invioce_detail);
+
+router.put("/update_status/invoice", guard, update_status);
 
 module.exports = router;
